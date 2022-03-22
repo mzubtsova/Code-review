1ba9a81c382f101befcb9f52f76db3659d109300
marina <mzubtsova@mail.ru>
2022-03-22 11:17:03 -0400

    add project3 code

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+// footer
+
+function Footer(){
+    return(
+        <ul>
+            <li><a href="https://junocollege.com/">Created by Marina Zubtsova at Juno College of Technology</a></li>
+        </ul>
+    )
+}
+
+export default Footer;
